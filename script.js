9ce5ff4aca5779b097aa3d1d29263e23ee7ccb65
Faithfuloscar <oscarmendez2003@outlook.com>
2024-12-02 14:44:50 -0800

    created functions initialize game and handlequareclick

--- script.js
+++ script.js
@@ -3,6 +3,22 @@
 const game ={
     currentPlayer: "x", // current player ("X", "O")
     board: Array(9).fill(null), //Represents the game board
-    gameOver:false, //tracks if the game is over
+    gameOver: false, //tracks if the game is over
     scores: {X:0, O:0}, // keeps track of the scores
 };
+
+function initializeGame() {
+    document.querySelectorAll(".game-square").forEach((square, index) => {
+        square.textContent = "";
+        square.classList.remove("highlight");
+        square.addEventListener("click",() => handleSquareClick(index));
+    });
+};
+
+function handleSquareClick(index) {
+    game.board[index] = game.currentPlayer;
+    const square = document.getElementById(`square-${ImageBitmapRenderingContext}`);
+    square.textContent = game.currentPlayer;
+}
+
+initializeGame();
