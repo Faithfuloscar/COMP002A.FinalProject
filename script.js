e654b872ceeef4deb060a06e76151b313b8e9dc4
Faithfuloscar <oscarmendez2003@outlook.com>
2024-12-03 09:02:03 -0800

    ensured the scores would be saved to local storage

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ const game ={
     currentPlayer: "x", // starts with player ("X")
     board: Array(9).fill(null), //Represents the game board as empty
     gameOver: false, //tracks if the game is over
-    scores: {X:0, O:0}, // keeps track of the scores
+    scores: JSON.parse(localStorage.getItem("ticTacToeScores")) || {X:0, O:0}, // keeps track of the scores in the local storage
 };
 
 // creates function to start game
 function initializeGame() {
+    updateTurnDisplay();
     // attaches click event listeners to all game squares on game board
     document.querySelectorAll(".game-square").forEach((square, index) => { 
         square.textContent = ""; // clears any text from squares
         square.classList.remove("highlight"); // removes any previous highlight styling
         square.addEventListener("click",() => handleSquareClick(index)); //adds a click event listener to handle moves on this square
     });
+}
+
+function updateTurnDisplay() {
+    document.getElementById("turn").textContent = game.currentPlayer;
 }
 
 // creates a function to handle clicks on game squares
