784403757cb1c92c3c4b64c3f9aef6780132bd8b
Faithfuloscar <oscarmendez2003@outlook.com>
2024-12-03 08:25:59 -0800

    added comments to functions initializegame and handlesquareclick

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 
 // Initialize the game state
 const game ={
-    currentPlayer: "x", // current player ("X", "O")
-    board: Array(9).fill(null), //Represents the game board
+    currentPlayer: "x", // starts with player ("X")
+    board: Array(9).fill(null), //Represents the game board as empty
     gameOver: false, //tracks if the game is over
     scores: {X:0, O:0}, // keeps track of the scores
 };
 
+// creates function to start game
 function initializeGame() {
-    document.querySelectorAll(".game-square").forEach((square, index) => {
-        square.textContent = "";
-        square.classList.remove("highlight");
-        square.addEventListener("click",() => handleSquareClick(index));
+    // attaches click event listeners to all game squares on game board
+    document.querySelectorAll(".game-square").forEach((square, index) => { 
+        square.textContent = ""; // clears any text from squares
+        square.classList.remove("highlight"); // removes any previous highlight styling
+        square.addEventListener("click",() => handleSquareClick(index)); //adds a click event listener to handle moves on this square
     });
 }
 
+// creates a function to handle clicks on game squares
 function handleSquareClick(index) {
-    game.board[index] = game.currentPlayer;
+    game.board[index] = game.currentPlayer; // update the board state with the current player's mark
+    // update the UI to show the current player's mark on the clicked square
     const square = document.getElementById(`square-${index}`);
     square.textContent = game.currentPlayer;
 }
 
+// starts the game
 initializeGame();
